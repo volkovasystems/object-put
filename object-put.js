7ec8df20f491f54d486632121c0077bb3f2b9760
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2013-09-01 23:56:08 +0800

    Improve put function.
    Added functionalities like custom options, fallback mechanisms, and
    multiple puts.

--- object-put.js
+++ object-put.js
@@ -1,3 +1,5 @@
+//Requires has function.
+
 Object.defineProperty( Object.prototype, "put",
 	{
 		"enumerable": false,
@@ -5,26 +7,58 @@ Object.defineProperty( Object.prototype, "put",
 		"writable": false,
 		"value": function put( key, value, options ){
 
+			if( arguments.length <= 2 ){
+				//It is safe to assume that the first
+				//	argument is an object.
+				var values = { };
+				if( typeof arguments[ 0 ] == "object" ){
+					values = arguments[ 0 ];
+				}else if( typeof arguments[ 0 ] == "string" ){
+					key = arguments[ 0 ];
+					values = undefined;
+				}
+
+				if( arguments[ 1 ] ){
+					options = arguments[ 1 ];
+				}
+
+				if( values ){
+					for( var property in values ){
+						this.put( property, values[ property ], options );
+					}
+					return this;	
+				}
+			}
+
 			if( !options ){
 				options = "public editable";
 			}
 
+			var overwrite;
 			if( typeof options == "string" ){
 				options = options.toLowerCase( );
 
-				var regexPublic = /public/;
-				var regexPrivate = /private/;
-				var regexReadOnly = /read-only/;
-				var regexWritable = /writable/;
-				var regexConstant = /constant/;
+				var regexPublic = /public/g;
+				var regexPrivate = /private/g;
+				var regexReadOnly = /read-only/g;
+				var regexWritable = /writable/g;
+				var regexConstant = /constant/g;
+				var regexOverwrite = /overwrite/g;
+				var regexDefault = /default/g;
+				var regexDefaultValue = /default(?:\:(number|string|boolean|object|function))?/;
+				var regexDefaultObject = /default:object(?:-([\w$][\w\d]*))?/;
+				var regexDefaultFunction = /default:function(?:-([\w$][\w\d]*))?/;
+				var regexFunction = /function-([\w$][\w\d]*\([^\(\)]*\)\s+\{.*\})/;
 
 				var configuration = { };
 				
 				if( regexPublic.test( options )
+					&& options.match( regexPublic ).length == 1
 					&& !regexPrivate.test( options ) )
 				{
 					configuration.enumerable = true;
 				}else if( regexPrivate.test( options )
+					&& options.match( regexPrivate ).length == 1
 					&& !regexPublic.test( options ) )
 				{
 					configuration.enumerable = false;	
@@ -33,10 +67,12 @@ Object.defineProperty( Object.prototype, "put",
 				}
 
 				if( regexReadOnly.test( options )
+					&& options.match( regexReadOnly ).length == 1
 					&& !regexWritable.test( options ) )
 				{
 					configuration.writable = false;
 				}else if( regexWritable.test( options )
+					&& options.match( regexWritable ).length == 1
 					&& !regexReadOnly.test( options ) )
 				{
 					configuration.writable = true;
@@ -45,6 +81,7 @@ Object.defineProperty( Object.prototype, "put",
 				}
 
 				if( regexConstant.test( options )
+					&& options.match( regexConstant ).length == 1
 					&& !regexReadOnly.test( options )
 					&& !regexWritable.test( options ) )
 				{
@@ -52,6 +89,65 @@ Object.defineProperty( Object.prototype, "put",
 					configuration.writable = false;
 				}else{
 					configuration.configurable = true;
+				}
+
+				if( value === undefined
+					&& regexDefaultValue.test( options )
+					&& options.match( regexDefault ).length == 1 )
+				{
+					var defaultType = options.match( regexDefaultValue )[ 1 ];
+					switch( defaultType ){
+						case "number":
+							value = 0;
+							break;
+
+						case "string":
+							value = "";
+							break;
+
+						case "boolean":
+							value = false;
+							break;
+
+						case "object":
+							var className = options.match( regexDefaultObject )[ 1 ];
+							if( blueprint ){
+								try{
+									value = new eval( className )( );
+								}catch( error ){
+									error.out( );
+									value = null;
+								}
+							}else{
+								value = { };
+							}
+							break;
+
+						case "function":
+							var functionName = options.match( regexDefaultFunction )[ 1 ];
+							if( functionName ){
+								var functionDefinition = options.match( regexFunction )[ 1 ];
+								if( functionDefinition ){
+									functionDefinition = "function " + functionDefinition;
+									eval( "value = " + functionDefinition + ";" );	
+								}
+							}
+							if( !value ){
+								value = function( ){ };
+							}
+							break;
+
+						default:
+							value = null;
+					}
+				}
+
+				if( regexOverwrite.test( options )
+					&& this.has( key ) )
+				{
+					overwrite = true;	
+				}else if( this.has( key ) ){
+					overwrite = false;
 				}
 
 				options = configuration;
@@ -70,10 +166,18 @@ Object.defineProperty( Object.prototype, "put",
 				}	
 			}
 
+			if( value === undefined ){
+				value = null;
+			}
+
 			options.value = value;
 
-			Object.defineProperty( this, key, options );
-
+			if( ( overwrite && typeof overwrite == "boolean" ) 
+				|| overwrite === undefined )
+			{
+				Object.defineProperty( this, key, options );	
+			}
+			
 			return this;			
 		}
 	} );
